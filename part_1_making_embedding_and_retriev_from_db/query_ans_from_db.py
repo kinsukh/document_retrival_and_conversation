b1fb7297fafcd1aa56973bbd58ebe10bdeadcce9
Kinsukh S. Sonbawne <95238196+kinsukh@users.noreply.github.com>
2024-09-15 18:25:57 +0530

    Update query_ans_from_db.py

--- part_1_making_embedding_and_retriev_from_db/query_ans_from_db.py
+++ part_1_making_embedding_and_retriev_from_db/query_ans_from_db.py
@@ -8,13 +8,9 @@ def embed_query(query):
     return query_embedding
 
 
-# Query the Pinecone index to find similar documents
 def search_in_pinecone(query_embedding, top_k=5):
-    # Perform the query and retrieve top_k most similar documents
     query_embedding = [query_embedding.tolist()]
     results = index.query(vector=query_embedding, top_k=top_k, include_metadata=True)
-    
-    # Return the list of matches
     return results['matches']
 
 
